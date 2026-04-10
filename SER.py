7912d82f3079fe0e0fb00fa63d1ac6b3de5b2597
Unnamalai Muthukumar <110679192+UnnamalaiMuthukumar@users.noreply.github.com>
2026-04-10 14:04:14 +0530

    SER.py

---
+++ SER.py
@@ -0,0 +1,251 @@
+from google.colab import drive
+drive.mount('/content/drive')
+Go to this URL in a browser: https://accounts.google.com/o/oauth2/auth?client_id=947318989803-6bn6qk8qdgf4n4g3pfee6491hc0brc4i.apps.googleusercontent.com&redirect_uri=urn%3aietf%3awg%3aoauth%3a2.0%3aoob&response_type=code&scope=email%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdocs.test%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive.photos.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fpeopleapi.readonly
+
+Enter your authorization code:
+··········
+Mounted at /content/drive
+
+[ ]
+!pip install soundfile
+Requirement already satisfied: soundfile in /usr/local/lib/python3.6/dist-packages (0.10.3.post1)
+Requirement already satisfied: cffi>=1.0 in /usr/local/lib/python3.6/dist-packages (from soundfile) (1.14.0)
+Requirement already satisfied: pycparser in /usr/local/lib/python3.6/dist-packages (from cffi>=1.0->soundfile) (2.19)
+
+[ ]
+import soundfile
+import numpy as np
+import librosa
+import glob
+import os
+from sklearn.model_selection import train_test_split
+
+# all emotions on RAVDESS dataset
+int2emotion = {
+    "01": "neutral",
+    "02": "calm",
+    "03": "happy",
+    "04": "sad",
+    "05": "angry",
+    "06": "fearful",
+    "07": "disgust",
+    "08": "surprised"
+}
+
+# we allow only these emotions
+AVAILABLE_EMOTIONS = {
+    "angry",
+    "sad",
+    "neutral",
+    "happy"
+}
+
+
+[ ]
+def extract_feature(file_name, **kwargs):
+    """
+    Extract feature from audio file `file_name`
+        Features supported:
+            - MFCC (mfcc)
+            - Chroma (chroma)
+            - MEL Spectrogram Frequency (mel)
+            - Contrast (contrast)
+            - Tonnetz (tonnetz)
+        e.g:
+        `features = extract_feature(path, mel=True, mfcc=True)`
+    """
+    mfcc = kwargs.get("mfcc")
+    chroma = kwargs.get("chroma")
+    mel = kwargs.get("mel")
+    contrast = kwargs.get("contrast")
+    tonnetz = kwargs.get("tonnetz")
+    with soundfile.SoundFile(file_name) as sound_file:
+        X = sound_file.read(dtype="float32")
+        sample_rate = sound_file.samplerate
+        if chroma or contrast:
+            stft = np.abs(librosa.stft(X))
+        result = np.array([])
+        if mfcc:
+            mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result = np.hstack((result, mfccs))
+        if chroma:
+            chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, chroma))
+        if mel:
+            mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, mel))
+        if contrast:
+            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+            result = np.hstack((result, contrast))
+        if tonnetz:
+            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+            result = np.hstack((result, tonnetz))
+    return result
+
+[ ]
+def load_data(test_size=0.2):
+    X, y = [], []
+    try :
+      for file in glob.glob("/content/drive/My Drive/wav/Actor_*/*.wav"):
+          # get the base name of the audio file
+          basename = os.path.basename(file)
+          print(basename)
+          # get the emotion label
+          emotion = int2emotion[basename.split("-")[2]]
+          # we allow only AVAILABLE_EMOTIONS we set
+          if emotion not in AVAILABLE_EMOTIONS:
+              continue
+          # extract speech features
+          features = extract_feature(file, mfcc=True, chroma=True, mel=True)
+          # add to data
+          X.append(features)
+          y.append(emotion)
+    except :
+         pass
+    # split the data to training and testing and return it
+    return train_test_split(np.array(X), y, test_size=test_size, random_state=7)
+
+[ ]
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.metrics import accuracy_score
+
+import os
+import pickle
+
+[ ]
+X_train, X_test, y_train, y_test = load_data(test_size=0.25)
+# print some details
+# number of samples in training data
+print("[+] Number of training samples:", X_train.shape[0])
+# number of samples in testing data
+print("[+] Number of testing samples:", X_test.shape[0])
+# number of features used
+# this is a vector of features extracted 
+# using utils.extract_features() method
+print("[+] Number of features:", X_train.shape[1])
+# best model, determined by a grid search
+model_params = {
+    'alpha': 0.01,
+    'batch_size': 256,
+    'epsilon': 1e-08, 
+    'hidden_layer_sizes': (300,), 
+    'learning_rate': 'adaptive', 
+    'max_iter': 500, 
+}
+# initialize Multi Layer Perceptron classifier
+# with best parameters ( so far )
+model = MLPClassifier(**model_params)
+
+# train the model
+print("[*] Training the model...")
+model.fit(X_train, y_train)
+
+# predict 25% of data to measure how good we are
+y_pred = model.predict(X_test)
+
+# calculate the accuracy
+accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+
+print("Accuracy: {:.2f}%".format(accuracy*100))
+
+# now we save the model
+# make result directory if doesn't exist yet
+if not os.path.isdir("result"):
+    os.mkdir("result")
+model = MLPClassifier(**model_params)
+
+# train the model
+print("[*] Training the model...")
+model.fit(X_train, y_train)
+
+# predict 25% of data to measure how good we are
+y_pred = model.predict(X_test)
+
+# calculate the accuracy
+accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+
+print("Accuracy: {:.2f}%".format(accuracy*100))
+
+from sklearn.metrics import classification_report 
+from sklearn.metrics import confusion_matrix 
+print(classification_report(y_test,y_pred))
+print(confusion_matrix(y_test,y_pred))
+
+    'alpha': 0.01,
+    'batch_size': 200,
+    'epsilon': 1e-08, 
+    'hidden_layer_sizes': (300,), 
+    'learning_rate': 'adaptive', 
+    'max_iter': 500, 
+}
+# initialize Multi Layer Perceptron classifier
+# with best parameters ( so far )
+m1 = MLPClassifier(**m_params)
+
+# train the model
+print("[*] Training the model...")
+m1.fit(X_train, y_train)
+
+# predict 25% of data to measure how good we are
+y_p = m1.predict(X_test)
+
+# calculate the accuracy
+accuracy = accuracy_score(y_true=y_test, y_pred=y_p)
+
+print("Accuracy: {:.2f}%".format(accuracy*100))
+
+from sklearn.metrics import classification_report 
+from sklearn.metrics import confusion_matrix 
+print(classification_report(y_test,y_p))
+print(confusion_matrix(y_test,y_p))
+
+mn_params = {
+    'alpha': 0.01,
+    'batch_size': 256,
+    'epsilon': 1e-08, 
+    'hidden_layer_sizes': (300,2), 
+    'learning_rate': 'adaptive', 
+    'max_iter': 500, 
+}
+# initialize Multi Layer Perceptron classifier
+# with best parameters ( so far )
+m2 = MLPClassifier(**mn_params)
+
+# train the model
+print("[*] Training the model...")
+m2.fit(X_train, y_train)
+
+# predict 25% of data to measure how good we are
+y_p2 = m2.predict(X_test)
+
+# calculate the accuracy
+accuracy = accuracy_score(y_true=y_test, y_pred=y_p2)
+
+print("Accuracy: {:.2f}%".format(accuracy*100))
+
+from sklearn.metrics import classification_report 
+from sklearn.metrics import confusion_matrix 
+print(classification_report(y_test,y_p2))
+print(confusion_matrix(y_test,y_p2))
+
+#output
+import os
+import wave
+import pickle
+from sys import byteorder
+from array import array
+from struct import pack
+from sklearn.neural_network import MLPClassifier
+
+
+[ ]
+#print("Please talk")
+filename = "D:\project_emotion/test2.wav"# record the file (start talking)
+    #record_to_file(filename)
+    # extract features and reshape it
+features = extract_feature(filename, mfcc=True, chroma=True, mel=True).reshape(1, -1)
+    # predict
+result = model.predict(features)[0]
+    # show the result !
+print("result:", result)
